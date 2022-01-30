429ed22a0e48b7e2cc8ab0c989a5e097a669f312
Maijs Garais <maireadsbooks@gmail.com>
2022-01-30 13:24:20 +0000

    change tile colours, handle keyboard input

--- js/main.js
+++ js/main.js
@@ -10,13 +10,41 @@ document.addEventListener("DOMContentLoaded", () => {
         keys[i].onclick = ({ target }) => {
             const key = target.getAttribute("data-key");
 
-            if (key == "enter") {
-                submitGuess();
-                return;
-            }
-            
-            updateGuess(key);
+            input(key);
         };
+    }
+
+    document.onkeyup = function (e) {
+        let code = e.code;
+        code = code.replace("Key", "");
+        code = code.toLowerCase();
+
+        input(code);
+    };
+
+    function input(code) {
+        if (code == "enter") {
+            submitGuess();
+            return;
+        }
+
+        updateGuess(code);
+    }
+
+    function getTileClass(letter, index) {
+        const contains = word.includes(letter);
+
+        if (!contains) {
+            return "incorrect";
+        }
+
+        const correctLetter = word.charAt(index);
+
+        if (letter === correctLetter) {
+            return "correct";
+        } else {
+            return "misplaced";
+        }
     }
 
     function submitGuess() {
@@ -26,6 +54,26 @@ document.addEventListener("DOMContentLoaded", () => {
             window.alert("Word must be 5 letters");
             return;
         }
+
+        const firstLetterId = (guesses.length - 1) * 5 + 1
+        const interval = 500;
+        let letters = [];
+        guess.forEach((letter, index) => {
+            const cl = getTileClass(letter, index);
+            const letterId = firstLetterId + index;
+            const letterEl = document.getElementById(String(letterId));
+            letters.push({l: letter, c: cl});
+            setTimeout(() => {
+                letterEl.classList.add("animate__flipInX");
+                letterEl.classList.add(cl);
+            }, interval * index);
+        });
+
+        setTimeout(() => {
+            for (let i = 0; i < letters.length; i++) {
+                document.getElementById(letters.at(i).l).classList.add(letters.at(i).c);
+            }
+        }, interval * 6);
 
         const currentWord = guess.join("");
         console.log(`current guess: ${currentWord}`);
@@ -65,6 +113,7 @@ document.addEventListener("DOMContentLoaded", () => {
         for (let i = 0; i < 30; i++) {
             let square = document.createElement("div");
             square.classList.add("square");
+            square.classList.add("animate__animated");
             square.setAttribute("id", i + 1);
             gameBoard.appendChild(square);
         }
