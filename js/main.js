33de16a4ee72eb73a3315bb9e3b5fb57a0363afd
Maijs Garais <maireadsbooks@gmail.com>
2022-01-30 11:19:32 +0000

    handle guesses against hardcoded word

--- js/main.js
+++ js/main.js
@@ -1,6 +1,64 @@
 document.addEventListener("DOMContentLoaded", () => {
     createSquares();
 
+    const keys = document.querySelectorAll(".keyboard-row button");
+    const word = "bonks";
+    let guesses = [[]];
+    let availableSpace = 1;
+
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].onclick = ({ target }) => {
+            const key = target.getAttribute("data-key");
+
+            if (key == "enter") {
+                submitGuess();
+                return;
+            }
+            
+            updateGuess(key);
+        };
+    }
+
+    function submitGuess() {
+        const guess = getCurrentGuess();
+
+        if (guess.length !== 5) {
+            window.alert("Word must be 5 letters");
+            return;
+        }
+
+        const currentWord = guess.join("");
+        console.log(`current guess: ${currentWord}`);
+
+        if (currentWord === word) {
+            window.alert("u got it");
+        }
+
+        if (guesses.length === 6) {
+            window.alert("noob lol");
+        }
+
+        guesses.push([]);
+    }
+
+    function getCurrentGuess() {
+        const numGuesses = guesses.length;
+        return guesses[numGuesses - 1];
+    }
+
+    function updateGuess(letter) {
+        const currentWord = getCurrentGuess();
+
+        if (currentWord && currentWord.length < 5) {
+            currentWord.push(letter);
+
+            const availableSpaceEl = document.getElementById(String(availableSpace));
+            availableSpace = availableSpace + 1;
+
+            availableSpaceEl.textContent = letter;
+        }
+    }
+
     function createSquares() {
         const gameBoard = document.getElementById("board");
 
@@ -11,4 +69,5 @@ document.addEventListener("DOMContentLoaded", () => {
             gameBoard.appendChild(square);
         }
     }
+
 });
